bd6781fe79d91dcff1a2e218cb43f44ffc56a469
kodom <sampaiorodrigomoreira@gmail.com>
2019-08-12 22:14:47 -0300

    exemplo lista

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Colecoes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            List<string> nomes = new List<string>();
+
+            nomes.Add("Rodrigo");
+            nomes.Add("Luana");
+            nomes.Add("Laura");
+            nomes.Add("lorena");
+
+            foreach(string item in nomes)
+            {
+                lista.Items.Add(item);
+            }
+
+            MessageBox.Show(nomes.Count.ToString());
+        }
+    }
+}
